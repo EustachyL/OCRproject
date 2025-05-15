ba6d43e6b9281a5053bc319b30fc0798581cb47d
EustachyL <eustachy.lis@gmail.com>
2025-05-15 22:54:52 +0200

    simple maui + gitignore

---
+++ OCR_Mauiapp/OCR_Mauiapp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace OCR_Mauiapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "OCR_Mauiapp" };
+        }
+    }
+}
